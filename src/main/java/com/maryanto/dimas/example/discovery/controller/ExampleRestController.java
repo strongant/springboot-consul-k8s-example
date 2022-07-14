4627604ddc1ceed7a48d4f9d96f019990682abaf
strongant.bai <strongant1994@gmail.com>
2022-07-15 01:35:09 +0800

    update consul url

--- src/main/java/com/maryanto/dimas/example/discovery/controller/ExampleRestController.java
+++ src/main/java/com/maryanto/dimas/example/discovery/controller/ExampleRestController.java
@@ -1,5 +1,6 @@
 package com.maryanto.dimas.example.discovery.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,19 @@ import java.util.Map;
 @RequestMapping("/api")
 public class ExampleRestController {
 
+    @Value("${server.port}")
+    private int serverPort;
+
     @GetMapping("/halo")
     public Map<String, Object> halo() {
         Map<String, Object> params = new HashMap<>();
         params.put("time", LocalDateTime.now());
         params.put("message", "Halo ini dari aplikasi springboot");
         return params;
+    }
+
+    @GetMapping("/call")
+    public String call() {
+        return "server:" + serverPort;
     }
 }
