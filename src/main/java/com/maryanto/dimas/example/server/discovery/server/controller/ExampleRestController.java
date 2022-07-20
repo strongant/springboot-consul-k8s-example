9d254f798474470e79c078097cd729da0447329c
strongant.bai <strongant1994@gmail.com>
2022-07-20 22:05:14 +0800

    update

--- src/main/java/com/maryanto/dimas/example/server/discovery/server/controller/ExampleRestController.java
+++ src/main/java/com/maryanto/dimas/example/server/discovery/server/controller/ExampleRestController.java
@@ -16,8 +16,8 @@ public class ExampleRestController {
     @Value("${server.port:8080}")
     private int serverPort;
 
-    @GetMapping("/halo")
-    public Map<String, Object> halo() {
+    @GetMapping("/server/hello")
+    public Map<String, Object> serverHalo() {
         Map<String, Object> params = new HashMap<>();
         params.put("time", LocalDateTime.now());
         params.put("message", "Halo ini dari aplikasi springboot");
